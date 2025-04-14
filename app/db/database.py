d58cc444ae7c781aa0c4f07df7561f1625744e8a
meirman_is_creator <sarsenbaymeyirman@gmail.com>
2025-04-15 00:40:31 +0500

    init the project second

--- app/db/database.py
+++ app/db/database.py
@@ -2,9 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
+import os
+
+# Определяем, запущено ли приложение в Docker или локально
+in_docker = os.environ.get('DOCKER_ENV', False)
+
+# Выбираем соответствующий URL для подключения к базе данных
+if in_docker:
+    db_url = settings.DATABASE_URL
+else:
+    db_url = settings.DATABASE_URL_LOCAL
 
 # Создаем подключение к базе данных
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(db_url)
 
 # Создаем фабрику сессий
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
